604ea99703320d7037a78fabdb36a91f9aa2b4ed
haneulOh <87457244+haneul5@users.noreply.github.com>
2023-07-27 17:49:36 +0900

    Create 오하늘.py

---
+++ [15주차] 코딩 연습/[BOJ] 17413 단어 뒤집기 2/오하늘.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+lst = list(input().strip())
+dq = deque(lst) # 데큐 사용
+ans = ''
+
+while dq:
+    if dq[0] == '<' : # 우선 조건 1 : 태그는 그대로
+        while 1 :
+            if dq[0] == '>':
+                ans += str(dq.popleft())
+                break
+            else : ans += str(dq.popleft())
+
+    if len(dq) == 0:
+        break
+
+    if dq[0] == ' ' : # 조건 2 : 공백의 경우 뒤집어준다
+        ans += str(dq.popleft()) # start 공백
+        tmp = "" # 임시 데큐 생성
+        while 1 :
+            if dq[0] == ' ' or dq[0] == '': # end 공백
+                ans += tmp[::-1]
+                if dq[0] == ' ' : ans += str(dq.popleft()) # 마지막 공백
+                break
+            else :
+                tmp += str(dq.popleft())
+    
+    if len(dq) == 0:
+        break
+    
+    else : # 조건3 : 평문
+        tmp = "" # 임시 데큐 생성
+        while 1 :
+            if dq[0] == ' ' or dq[0] =='<' or dq[0] == '':
+                ans += tmp[::-1]
+                if dq[0] == ' ' : ans += str(dq.popleft()) # 마지막 공백
+                break
+            else :
+                tmp += str(dq.popleft())
+
+print(ans)
