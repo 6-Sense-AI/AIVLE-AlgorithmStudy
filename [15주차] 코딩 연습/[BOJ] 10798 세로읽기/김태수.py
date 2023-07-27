55f24b1707f6a4c7d226c7fc6727af26243fe302
kimgitt <124108710+kimgitt@users.noreply.github.com>
2023-07-27 16:23:54 +0900

    Create 김태수.py

---
+++ [15주차] 코딩 연습/[BOJ] 10798 세로읽기/김태수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+graph = [input() for _ in range(5)]
+
+max = len(graph[0])
+for i in range(1, 5):
+    com = len(graph[i])
+    if com >= max:
+        max = com
+
+# print(max)
+for i in range(max):
+    for j in range(5):
+        # if i < len(graph[j]):
+        #     print(graph[j][i], end="")
