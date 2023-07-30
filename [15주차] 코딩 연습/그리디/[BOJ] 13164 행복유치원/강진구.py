882f1e4383fdf35d3fd4239e61c68827fce3e524
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-07-30 15:29:45 +0900

    Update 강진구.py
    성공!

--- [15주차] 코딩 연습/그리디/[BOJ] 13164 행복유치원/강진구.py
+++ [15주차] 코딩 연습/그리디/[BOJ] 13164 행복유치원/강진구.py
@@ -1,5 +1,29 @@
+# 바로 옆에 애들끼리 비교해서 전체에서 큰걸 빼준다는 생각입니다
+
 n, k = map(int, input().split())
 
-lst = map(int, input().split())
+child = list(map(int, input().split()))
 
-cost = int(10e9)
+def compare(lst):
+    x = 0
+    idx_lst = []
+    for i, l in enumerate(lst):
+        if i == 0:
+            x = l
+            continue
+        else:
+            c = (l - x)
+            idx_lst.append((i, c))
+            x = l
+        
+    return idx_lst
+
+idx = compare(child)
+
+sort_idx = sorted(idx, key = lambda x : -x[1])
+
+cost = child[-1] - child[0]
+for _,c in sort_idx[:k-1]:
+    cost -= c
+
+print(cost)
