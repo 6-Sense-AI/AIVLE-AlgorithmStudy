97a6f6e42dd99b36311a48b5e0327ed7e462fcb3
facule <dhgksmf423@gmail.com>
2023-08-15 19:48:50 +0900

    Fix code : 오하늘 PGS Level2 타겟넘버

--- [16주차] BFS and DFS/[PGS] LV2 타겟넘버/오하늘 PGS LV2 타겟넘버.py
+++ [16주차] BFS and DFS/[PGS] LV2 타겟넘버/오하늘 PGS LV2 타겟넘버.py
@@ -1,1 +1,17 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/43165
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
+## 사용방법 : list
+
+def solution(numbers, target):
+    answer = 0
+    
+    arr = [0] # 처음에 0을 넣어주지 않으면 arr값이 없어서 for문이 돌지 않음
+    for i in numbers:
+        tmp = []
+        for j in arr:
+            tmp.append(j+i) # +
+            tmp.append(j-i) # -
+        arr = tmp # arr에 중도 저장 이러면 arr는 마지막의 모든 경우의 수를 알게 됨
+    answer = arr.count(target) # count 사용! 타겟과 같은 수의 갯수 반환해줌
+    return answer
