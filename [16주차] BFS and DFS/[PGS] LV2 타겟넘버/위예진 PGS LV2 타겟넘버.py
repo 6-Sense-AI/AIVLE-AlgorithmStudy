f4291d9ede78ecf918bdb802d1b20726517a48c0
bokkuembab <weestudy77@gmail.com>
2023-08-16 11:34:12 +0900

    Update code : 위예진 PGS LV2 타겟넘버

--- [16주차] BFS and DFS/[PGS] LV2 타겟넘버/위예진 PGS LV2 타겟넘버.py
+++ [16주차] BFS and DFS/[PGS] LV2 타겟넘버/위예진 PGS LV2 타겟넘버.py
@@ -1,3 +1,9 @@
+# 현재 범위 밖에서 선언된 변수를 '읽기'는 가능하지만, '변경'은 불가능하다!
+# 따라서, 아래와 같이 선언을 해줘야 함!!
+# global: 전역변수로 선언
+# nonlocal: 전역변수 X, 현재 범위의 지역변수도 아닐 때 선언
+# 참고 (https://juhi.tistory.com/6)
+
 def solution(numbers, target):
     ans = 0
     
@@ -5,7 +11,7 @@ def solution(numbers, target):
     # -> bfs/dfs 둘 다 상관 없을 듯
     # now_idx: 현재 트리 깊이, now_sum: 현재까지의 합
     def make_target(now_idx, now_sum):
-        nonlocal ans
+        nonlocal ans    # 전역 변수가 아닌데, 현재 함수 내의 지역변수도 아닐 때는 nonlocal로 선언!
         
         # 숫자의 끝에 도달했으면, 종료
         if now_idx == len(numbers):
