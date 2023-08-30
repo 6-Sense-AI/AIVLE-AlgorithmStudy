3d4b2719db7d19fd60168032590e7aec68d0f4ff
facule <dhgksmf423@gmail.com>
2023-08-30 21:33:44 +0900

    Fix code : 오하늘 BOJ 골드5 15486 퇴사2

--- [19주차] 구현/대면문제/[BOJ] 골드5 15486 퇴사2/오하늘 BOJ 골드5 15465 퇴사2.py
+++ [19주차] 구현/대면문제/[BOJ] 골드5 15486 퇴사2/오하늘 BOJ 골드5 15465 퇴사2.py
@@ -5,13 +5,18 @@ input = sys.stdin.readline
 
 day = int(input()) #날짜
 info = []
+tmp = []
 for i in range(day):
     info.append(list(map(int, input().split()))) # 정보
+    if  i + info[i][0] > day : tmp.append(i)
 
 dp = [0] * day
 result = [0]*day
 
+
+# 첫번째 방법 - 예제 4 틀림 가능한 모든 인덱스는 돌아봐야 함
 for i in range(day): # 수익구하기
+    if i + info[i][0] > day : break # 6과 7의 경우
     k = i
     temp = 0
     while 1 :
@@ -26,3 +31,14 @@ for i in range(day): # 수익구하기
     result[i] = temp
 
 print(max(result))
+
+# 두번쨰 방법 전략
+# 가능한 인덱스는 모두 구해놓음
+for i in range(day): # 수익구하기
+    if i + info[i][0] > day : break # 6과 7의 경우
+    
+    for j in range(i,len(tmp)): # 가능한 모든 수를 구하고 최대 출력
+        # 이거로 최대를 계속 갱신하는 점화식을 세울 것임.
+        dp[i] = max(dp[i], info[i][1]+info[j]) # 이전 vs 다음꺼랑 비교
+    
+print(dp[day-1])
