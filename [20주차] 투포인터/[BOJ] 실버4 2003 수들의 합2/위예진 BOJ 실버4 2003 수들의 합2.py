f9b46f2c5d81edb7b0e39641ef97d9d3404decb3
bokkuembab <weestudy77@gmail.com>
2023-09-10 20:30:15 +0900

    Fix code : 위예진 BOJ 실버4 2003 수들의 합2

--- [20주차] 투포인터/[BOJ] 실버4 2003 수들의 합2/위예진 BOJ 실버4 2003 수들의 합2.py
+++ [20주차] 투포인터/[BOJ] 실버4 2003 수들의 합2/위예진 BOJ 실버4 2003 수들의 합2.py
@@ -6,7 +6,7 @@ n, target = map(int, input().split())
 nlist = list(map(int, input().split()))    # 수열
 ans = 0    # 경우의 수
 
-left, right = 0, 1
+left, right = 0, 0
 while left <= right and right <= n:
     now = sum(nlist[left:right])    # 현재 합
     
@@ -19,3 +19,22 @@ while left <= right and right <= n:
         left += 1
 
 print(ans)
+
+########################### 의문점 해결 ###########################
+# 현재 합 부분을 아래와 같이 설정하면, 아래 테스트 케이스 충족 X
+# now = sum(nlist[left:right+1])    # 현재 합
+
+# (반례)
+# 입력
+# 4 1
+# 1 2 3 1
+
+# 출력
+# 1
+
+# 답
+# 2
+
+# 이유는, left와 right가 만나고, 
+# 총합이 0이 되어 다시 시작하는 부분이 필요하기 때문!
+# 따라서 [left:right+1] 와 같이 무조건 값 하나를 선택하게 되면 정답에 도달할 수 X
