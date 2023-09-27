c09d417cc7d79c21df24efec9e39e4d8ad533acd
박세은 <seeunbag30@gmail.com>
2023-09-27 16:31:58 +0900

    Create code : 박세은 PGS LV2 요격시스템.py

--- [23주차] 기출/대면문제/[PGS] LV2 요격시스템/박세은 PGS LV2 요격시스템.py
+++ [23주차] 기출/대면문제/[PGS] LV2 요격시스템/박세은 PGS LV2 요격시스템.py
@@ -1,1 +1,12 @@
-
+def solution(targets):
+    targets.sort()
+    chain = [[] for _ in range(len(targets))]
+    
+    for i in range(len(targets)):
+        for j in range(i+1, len(targets)):
+            if targets[i][1] > targets[j][0]:
+                chain[i].append(targets[j])
+            
+            else: break
+    
+    
