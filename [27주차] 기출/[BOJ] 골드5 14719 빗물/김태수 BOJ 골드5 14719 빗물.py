3834de2c5875366c94a7715dfe3f1fe8c4b1f68b
kimgitt <taesoo691@gmail.com>
2023-10-28 16:02:08 +0900

    Create code : 김태수 BOJ 골드5 14719 빗물

--- [27주차] 기출/[BOJ] 골드5 14719 빗물/김태수 BOJ 골드5 14719 빗물.py
+++ [27주차] 기출/[BOJ] 골드5 14719 빗물/김태수 BOJ 골드5 14719 빗물.py
@@ -1,1 +1,32 @@
-https://www.acmicpc.net/problem/14719
+# 답이 0인 예외를 제거 못함
+# import sys
+# input=sys.stdin.readline
+
+# h,w = map(int,input().split())
+# block = list(map(int,input().split()))
+
+# cnt = 0
+# for i in range(w-1):
+#     if block[i] > block[i+1]:
+#         max = block[i]
+#         cnt += block[i]-block[i+1]
+#     else:
+#         max = block[i+1]
+#         cnt += block[i+1]-block[i+1]
+# print(cnt)
+
+import sys
+input=sys.stdin.readline
+
+h,w = map(int,input().split())
+block = list(map(int,input().split()))
+cnt = 0
+
+for i in range(1, w-1):
+    left = max(block[ :i])
+    right = max(block[i+1: ])
+    m = min(left, right)
+    if m>block[i]:
+        cnt += m - block[i]
+
+print(cnt)
