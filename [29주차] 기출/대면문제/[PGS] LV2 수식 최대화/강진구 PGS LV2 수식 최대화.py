73af2d428f2e5b3d4967b0b036aa3fc72a1e5e62
박세은 <seeunbag30@gmail.com>
2023-11-09 22:45:39 +0900

    Create file : '대면문제' file 생성 및 파일들 이동

---
+++ [29주차] 기출/대면문제/[PGS] LV2 수식 최대화/강진구 PGS LV2 수식 최대화.py
@@ -0,0 +1,49 @@
+# 아직 못품...
+from collections import deque
+def solution(expression):
+    answer = 0
+    exp = []
+    n = ''
+    for e in expression:
+        if e.isdigit():
+            n+=e
+        else:
+            exp.append(int(n))
+            exp.append(e)
+            n = ''
+    exp.append(int(n))
+    i = 0
+    que = deque(exp)
+    while que:
+        x = que.popleft()
+        if x == '*':
+            num = que[-1]*que[0]
+            que.pop()
+            que.popleft()
+            que.append(num)
+        else:
+            if '*' in que:
+                que.append(x)
+            else:
+                if x == '+':
+                    num = que[-1]+que[0]
+                    que.pop()
+                    que.popleft()
+                    que.append(num)
+                else:
+                    if '+' in que:
+                        que.append(x)
+                    else:
+                        if x == '-':
+                            num = que[-1]-que[0]
+                            que.pop()
+                            que.popleft()
+                            que.append(num)
+                        else:
+                            if '-' in que:
+                                que.append(x)
+                            else:
+                                answer = max(answer,abs(x))
+        
+    
+    return answer 
