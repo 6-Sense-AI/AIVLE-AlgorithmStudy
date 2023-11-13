0f12db654d8ef603a6296b367542085655b9da99
bokkuembab <weestudy77@gmail.com>
2023-11-13 15:42:56 +0900

    Fix code : 위예진 PGS LV2 행렬 테두리 회전하기

--- [29주차] 기출/[PGS] LV2 행렬 테두리 회전하기/위예진 PGS LV2 행렬 테두리 회전하기.py
+++ [29주차] 기출/[PGS] LV2 행렬 테두리 회전하기/위예진 PGS LV2 행렬 테두리 회전하기.py
@@ -1,15 +1,13 @@
-# 아직 해결 X
-
 from collections import deque
 
 def solution(rows, cols, queries):
     ans = []
     rotated = [[False] * (cols + 1) for _ in range(rows + 1)]
     moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]    # 시계방향
-    m = 0
     
     for r1, c1, r2, c2 in queries:
         nr, nc = r1, c1
+        m = 0
         
         if not rotated[nr][nc]:
             rotated[nr][nc] = (nr - 1) * cols + nc
@@ -17,10 +15,10 @@ def solution(rows, cols, queries):
         tmp = rotated[nr][nc]
         
         while True:
-            dr, dc = moves[m][0], moves[m][1]
+            dr, dc = moves[m]
             mr, mc = nr + dr, nc + dc
             
-            if r1 <= mr <= r2 and c1 <= mc < c2:    # 범위 내 확인
+            if r1 <= mr <= r2 and c1 <= mc <= c2:    # 범위 내 확인
                 nr, nc = mr, mc    # 이동
                 
                 if not rotated[nr][nc]:    # 이동한 적 없다면,
