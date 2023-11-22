eb95542dee84daca0d151534e14b2e09691f75b7
facule <dhgksmf423@gmail.com>
2023-11-22 23:39:26 +0900

    Fix code : 오하늘 PGS LV2 구명보트

--- [30주차] 기출/[PGS] LV2 구명보트/오하늘 PGS LV2 구명보트.py
+++ [30주차] 기출/[PGS] LV2 구명보트/오하늘 PGS LV2 구명보트.py
@@ -1,1 +1,20 @@
-https://school.programmers.co.kr/learn/courses/30/lessons/42885
+from collections import deque
+def solution(people, limit):
+    # 무게 제한 > 최대몸무게
+    # 무거운 사람 구출하되 가장 가벼운 사람 순으로 빼내면 된다
+    # 데큐 안쓰면 시초남
+    
+    answer = 0
+    # p = deque(people)
+    # p.sort()
+    p = deque(sorted(people))
+    
+    while 1 :
+        if not p : break # p없으면
+        tmp = p.pop()
+        # len넣어줘야딤
+        if len(p) > 0 and tmp + p[0] <= limit: # 가벼운애 빼도 ㄱㅊ으면 빼
+            p.popleft()
+        answer +=1
+    
+    return answer
