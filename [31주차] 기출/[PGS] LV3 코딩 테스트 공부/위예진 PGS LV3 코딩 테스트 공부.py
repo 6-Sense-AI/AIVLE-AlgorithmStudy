bc153c87e20a48cecf4f4f4f2b193619f0a61566
bokkuembab <weestudy77@gmail.com>
2023-11-29 10:57:45 +0900

    Fix code: 위예진 PGS LV3 코딩 테스트 공부

--- [31주차] 기출/[PGS] LV3 코딩 테스트 공부/위예진 PGS LV3 코딩 테스트 공부.py
+++ [31주차] 기출/[PGS] LV3 코딩 테스트 공부/위예진 PGS LV3 코딩 테스트 공부.py
@@ -1,25 +1,33 @@
-# 못 품,,
+# 못품,,,,DP 문제!
+# 풀이 봤음!
 
 def solution(alp, cop, problems):
-    ans = 0
     max_alp, max_cop = 0, 0
-    min_alp, min_cop = 150, 150
     for a, c, _, _, _ in problems:
-        max_alp, min_alp = max(a, max_alp), min(a, min_alp)
-        max_cop, min_cop = max(c, max_cop), min(c, min_cop)
+        max_alp = max(a, max_alp)
+        max_cop = max(c, max_cop)
     
-    # 아무 문제도 풀 수 없으면, 하나씩 올림
-    if alp < min_alp:
-        c = min_alp - alp
-        alp += c
-        ans += c    # 풀이 시간 추가
-    if cop < min_cop:
-        c = min_cop - cop
-        cop += c
-        ans += c    # 풀이 시간 추가
-    
-    # 모든 문제를 풀 수 있을 때까지 반복함
-    while alp < max_alp or cop < max_cop:
-        # 
-        
-    return ans
+    # 가장 작은 노력부터 시작해서 dp 채워야 하므로
+    alp = min(max_alp, alp)
+    cop = min(max_cop, cop)
+    # 문제 안풀고 단순히 각 노력을 올리기만 했을 때의 최대값
+    max_cost = 100 * (max_alp + max_cop)
+
+    dp = [[max_cost + 1 for _ in range(max_cop + 1)] for _ in range(max_alp + 1)]
+    dp[alp][cop] = 0
+
+    # max_alp, max_cop까지 dp 리스트 채움
+    for i in range(alp, max_alp + 1):
+        for j in range(cop, max_cop + 1):
+            if i + 1 <= max_alp:
+                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
+            if j + 1 <= max_cop:
+                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
+            
+            for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+                if i >= alp_req and j >= cop_req:
+                    nxt_alp = min(max_alp, i + alp_rwd)
+                    nxt_cop = min(max_cop, j + cop_rwd)
+                    dp[nxt_alp][nxt_cop] = min(dp[nxt_alp][nxt_cop], dp[i][j] + cost)
+     
+    return dp[-1][-1]
