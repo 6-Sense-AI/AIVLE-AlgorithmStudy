b8eae8e618e7780a1c8325f4f2519c5690b6f9a3
bokkuembab <weestudy77@gmail.com>
2023-11-29 12:39:49 +0900

    Fix code: 위예진 BOJ 골드4 1339 단어 수학

--- [31주차] 기출/[BOJ] 골드4 1339 단어 수학/위예진 BOJ 골드4 1339 단어 수학.py
+++ [31주차] 기출/[BOJ] 골드4 1339 단어 수학/위예진 BOJ 골드4 1339 단어 수학.py
@@ -1,50 +1,37 @@
-# 테케는 다 맞는데 제출하면 처음부터 틀림ㅠㅠ
+# 해결
 
+from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-words = [list(input().rstrip()) for _ in range(n)]
-n10 = ['' for _ in range(8)]    # 자릿수에 해당하는 알파벳
-alp_num = dict()    # 알파벳 등장 횟수
+words = []
+weights = defaultdict(int)    # 각 알파벳의 가중치 저장
 alp_to_num = dict()    # 각 알파벳에 대응하는 숫자 사전
 
-# 자릿수 배정
-for w in words:
+# 단어 저장하며, 알파벳 가중치 저장
+for _ in range(n):
+    w = list(input().rstrip())
+    words.append(w)
     for i in range(len(w)):
-        alp = w[-(i+1)]
+        weights[w[i]] += 10 ** (len(w) - i)
 
-        if alp not in alp_num:    # 처음 등장
-            alp_num[alp] = 1
-        else:
-            alp_num[alp] += 1
-        n10[i] += alp
-
-# 순서 배정 (자릿수, 등장 횟수 순으로)        
-ord_alp = ''
-for w in n10:
-    if len(w) > 2:
-        ord = []
-        for alp in w:
-            ord.append((alp_num[alp], alp))
-        ord = sorted(ord, key=lambda x: x[0])
-        for _, alp in ord:
-            ord_alp += alp
-    else:
-        ord_alp += w
-n10 = ''.join(n10)[::-1]
+# 가중치순으로 알파벳 역순 정렬
+weights = sorted(weights.items(), key=lambda x: -x[1])
 
 # 알파벳 대응 사전 채우기
 ni = 9
-for alp in ord_alp[::-1]:
-    if not alp in alp_to_num:
-        alp_to_num[alp] = ni
-        ni -= 1
+for alp, _ in weights:
+    alp_to_num[alp] = ni
+    ni -= 1
 
 # 숫자 연산
 ans = 0
 for w in words:
-    for i, alp in enumerate(w[::-1]):
-        ans += (alp_to_num[alp] * (10 ** i))
+    w_num = ''
+    for alp in w:
+        w_num += str(alp_to_num[alp])
+
+    ans += int(w_num)
 
 print(ans)
