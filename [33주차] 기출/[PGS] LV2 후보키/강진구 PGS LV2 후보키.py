7a3a3d5d4e8e61325c9c520eb5cf3b8b531a71d3
jingu1138 <jingu4055@gmail.com>
2023-12-12 15:26:13 +0900

    Create code: 강진구 PGS LV2 후보키

--- [33주차] 기출/[PGS] LV2 후보키/강진구 PGS LV2 후보키.py
+++ [33주차] 기출/[PGS] LV2 후보키/강진구 PGS LV2 후보키.py
@@ -1,1 +1,22 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42890
+# 집합끼리 비교할때 오류가 났는지 히든에서 틀려서 정답을 참고했습니다.
+from itertools import combinations as cb
+def solution(relation):
+    
+    col = len(relation[0])
+    cb_lst = []
+    for i in range(1,col+1):
+        cb_lst.extend(cb(range(col),i))
+    uni = []
+    for c in cb_lst:
+        tmp = [tuple(item[key] for key in c) for item in relation]
+        
+        if len(set(tmp)) == len(relation):
+            flag = True
+            
+            for u in uni:
+                if set(u).issubset(set(c)):
+                    flag = False
+                    break
+            if flag:
+                uni.append(c)
+    return len(uni)
