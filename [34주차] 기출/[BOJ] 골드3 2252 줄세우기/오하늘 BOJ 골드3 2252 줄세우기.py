5d9f3370ea3d4f63ca5c2a123d99dd023f8b1256
facule <dhgksmf423@gmail.com>
2023-12-13 16:05:00 +0900

    Fix2 code : 오하늘 BOJ 2252 줄세우기

--- [34주차] 기출/[BOJ] 골드3 2252 줄세우기/오하늘 BOJ 골드3 2252 줄세우기.py
+++ [34주차] 기출/[BOJ] 골드3 2252 줄세우기/오하늘 BOJ 골드3 2252 줄세우기.py
@@ -1,24 +1,32 @@
 import sys
 input = sys.stdin.readline
-# 시도1 : 스위치 방법. 나열해놓고, 그 순서가 등장하면 자리를 바꾼다.
-# 결과 시간초과
+from collections import deque
+# 시도2 : 위상정렬 공부 이후
+
 n, m =map(int, input().split())
 
-arr = []
-for i in range(n):
-    arr.append(i+1)
+in_arr = [0] * (n+1) #진입차수 리스트
+info = [[] for i in range(n+1)] # 배열 리스트
 
 for i in range(m):
-    a, b = map(int, input().split())
-    t1 = arr.index(a) # 전
-    t2 = arr.index(b) # 후
+    a, b = map(int,input().split())
+    info[a].append(b) # a->b 이동
+    in_arr[b] += 1
 
-    if t1 > t2 :
-        tmp = arr[t1]
-        arr[t1] = arr[t2]
-        arr[t2] = tmp
+def topology_sort(): # 위상 정렬
+    ans = []
+    q=deque()
+    for i in range(1, n+1):
+        if in_arr[i] == 0: # 시작점
+            q.append(i)
+    while q: # q가 다 사라질때 까지
+        now = q.popleft()
+        ans.append(now)
+        for i in info[now]:
+            in_arr[i] -= 1 # 진입차수 줄여
+            if in_arr[i] == 0:
+                q.append(i) # q에 넣기
+    for i in ans:
+        print(i, end=' ')
 
-for i in range(len(arr)):
-    if i == len(arr)-1:
-        print(arr[i])
-    else: print(arr[i], end=' ')
+topology_sort()
