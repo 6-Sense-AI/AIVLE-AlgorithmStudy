4f0e7f717918bb44819f6f3b837ab5ec6c3a6fe1
kimgitt <taesoo691@gmail.com>
2023-12-13 16:22:54 +0900

    Create code : 김태수 BOJ 실버4 11399 ATM

---
+++ [34주차] 기출/[BOJ] 실버4 11399 ATM/김태수 BOJ 실버4 11399 ATM.py
@@ -0,0 +1,13 @@
+import sys
+input =sys.stdin.readline
+
+n = int(input())
+p = list(map(int,input().split()))
+
+p.sort()
+answer = 0
+
+for i in range(1, n+1):
+    answer += sum(p[:i])
+
+print(answer)
