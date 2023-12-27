6bc46577c48954842449416e458694e33b6b67a3
facule <dhgksmf423@gmail.com>
2023-12-27 16:23:20 +0900

    Fix code : 오하늘 BOJ 2705 팰린드롬 파티션

--- [35주차] 기출/[BOJ] 2705 팰린드롬 파티션/오하늘 BOJ 팰린드롬 파티션.py
+++ [35주차] 기출/[BOJ] 2705 팰린드롬 파티션/오하늘 BOJ 팰린드롬 파티션.py
@@ -1,1 +1,22 @@
-https://www.acmicpc.net/problem/2705
+import sys
+
+test = int(input()) # testcase
+#try 1: 합분해 + 펠린드롬 체크
+
+for _ in range(test) :
+    def isP(n): # 팰린드롬 확인
+        if n == n[::-1] :
+            return True
+        else : return False
+
+    n = int(input()) # 수
+
+    # 합분해 알고리즘 (DP)
+    # dp = [[0] * i for i in range(n+1)]
+    # dp[0][0] = 1
+    # for i in range(n): # i
+    #     for j in range
+        
+
+        
+    print(isP(str(131)))
